3f0f759dc721508a4195a0719c58b28d6f384b47
Mateo DM <104037425+mateodm@users.noreply.github.com>
2022-08-01 20:23:06 -0300

    segundo commit

--- node.js
+++ node.js
@@ -1,7 +1,14 @@
-let marginAlea = (Math.random()*300)
-let marginAlea2 = (Math.random()*250)
+
 
 function evento() {
+    let marginAlea = (Math.random()*800)
+    let marginAlea2 = (Math.random()*700)
+    let marginAlea3 = (Math.random()*700)
+    let marginAlea4 = (Math.random()*800)
     button.style.marginLeft = marginAlea + "px";
     button.style.marginTop = marginAlea2 + "px";
+    button.style.marginBottom = marginAlea3 + "px";
+    button.style.marginRight = marginAlea4 + "px";
+    return true;
 }
+
