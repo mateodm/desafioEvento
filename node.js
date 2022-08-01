9a1564a943e9a60bfe36001c9c6fb49c62182c8e
Mateo DM <104037425+mateodm@users.noreply.github.com>
2022-08-01 01:21:17 -0300

    primer commit

---
+++ node.js
@@ -0,0 +1,7 @@
+let marginAlea = (Math.random()*300)
+let marginAlea2 = (Math.random()*250)
+
+function evento() {
+    button.style.marginLeft = marginAlea + "px";
+    button.style.marginTop = marginAlea2 + "px";
+}
