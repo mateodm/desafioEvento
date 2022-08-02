ec1e0f3b3eb1d803f6fe1aebd47fd6c9101eb328
Mateo DM <104037425+mateodm@users.noreply.github.com>
2022-08-01 21:29:11 -0300

    tercer commit

--- node.js
+++ node.js
@@ -1,6 +1,8 @@
-
+let puntaje
+puntaje = 0
 
 function evento() {
+    puntaje++;
     let marginAlea = (Math.random()*800)
     let marginAlea2 = (Math.random()*700)
     let marginAlea3 = (Math.random()*700)
@@ -9,6 +11,13 @@ function evento() {
     button.style.marginTop = marginAlea2 + "px";
     button.style.marginBottom = marginAlea3 + "px";
     button.style.marginRight = marginAlea4 + "px";
-    return true;
+
+}
+function calculoPuntos() {
+    document.getElementById("score").innerHTML = puntaje;
+    if (puntaje == 8) {
+        alert("Felicidades, alcanzaste la puntuación máxima")
+    }
 }
 
+calculoPuntos()
