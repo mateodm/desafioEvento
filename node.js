7a24acf3470a23cd74a8b6bfdbb4d1f7198ee2e6
Mateo DM <104037425+mateodm@users.noreply.github.com>
2022-08-01 22:22:37 -0300

    score

--- node.js
+++ node.js
@@ -1,23 +1,20 @@
-let puntaje
-puntaje = 0
+let puntaje = 0
+
 
 function evento() {
     puntaje++;
+    document.getElementById("score").innerHTML = puntaje;
+    if (puntaje == 8) {
+        alert("Felicidades, alcanzaste la puntuación máxima")
+        return (puntaje = 0)
+    }
     let marginAlea = (Math.random()*800)
-    let marginAlea2 = (Math.random()*700)
-    let marginAlea3 = (Math.random()*700)
+    let marginAlea2 = (Math.random()*800)
+    let marginAlea3 = (Math.random()*800)
     let marginAlea4 = (Math.random()*800)
     button.style.marginLeft = marginAlea + "px";
     button.style.marginTop = marginAlea2 + "px";
     button.style.marginBottom = marginAlea3 + "px";
     button.style.marginRight = marginAlea4 + "px";
-
-}
-function calculoPuntos() {
-    document.getElementById("score").innerHTML = puntaje;
-    if (puntaje == 8) {
-        alert("Felicidades, alcanzaste la puntuación máxima")
-    }
 }
 
-calculoPuntos()
